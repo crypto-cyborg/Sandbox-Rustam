d240b9ebaf983e9d3791e2e45b45fe9842197d0f
Rustam <rustam.niftaliev97@gmail.com>
2025-03-12 13:09:06 +0400

    Fix bugs

--- Sandbox.Shared/DTOs/OrderDto.cs
+++ Sandbox.Shared/DTOs/OrderDto.cs
@@ -1,33 +1,18 @@
-using Newtonsoft.Json;
 
 namespace Sandbox.Shared.DTOs
 {
     public class OrderDto
     {
-        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public Guid? Id { get; set; }
         public Guid WalletId { get; set; }
         public string Symbol { get; set; }
         public decimal Quantity { get; set; }
-        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public decimal? Price { get; set; }
         public string OrderType { get; set; } 
-        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
-        
         public string? Status { get; set; }    
         public string Direction { get; set; } 
-        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public decimal? Leverage { get; set; } 
     }
 
-    // public class CreateOrderDto
-    // {
-    //     public Guid WalletId { get; set; }
-    //     public string Symbol { get; set; }
-    //     public decimal Quantity { get; set; }
-    //     public string OrderType { get; set; } 
-    //     public string Direction { get; set; } 
-    //     public decimal? Leverage { get; set; } 
-    //     public decimal? Price { get; set; }
-    // }
+ 
 }
